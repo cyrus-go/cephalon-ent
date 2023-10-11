e3448739ecd2cd2b8ecbac084055c3b135d0c506
cyrus@cephalon.ai <cyrus@cephalon.ai>
2023-10-11 16:49:18 +0800

    fix: bill type add time plan hour to month

--- pkg/enums/bill_way.go
+++ pkg/enums/bill_way.go
@@ -3,19 +3,22 @@ package enums
 type BillWay string
 
 const (
-	BillWayUnknown             BillWay = "unknown"
-	BillWayRechargeWechat      BillWay = "recharge_wechat"
-	BillWayRechargeAlipay      BillWay = "recharge_alipay"
-	BillWayMissionTime         BillWay = "mission_time"
-	BillWayMissionTimePlan     BillWay = "mission_time_plan"
-	BillWayMissionCount        BillWay = "mission_count"
-	BillWayMissionHold         BillWay = "mission_hold"
-	BillWayMissionVolume       BillWay = "mission_volume"
-	BillWayActiveRegister      BillWay = "active_register"
-	BillWayActiveShare         BillWay = "active_share"
-	BillWayActiveRecharge      BillWay = "active_recharge"
-	BillWayTransferManual      BillWay = "transfer_manual"
-	BillWayFirstInviteRecharge BillWay = "first_invite_recharge"
+	BillWayUnknown              BillWay = "unknown"
+	BillWayRechargeWechat       BillWay = "recharge_wechat"
+	BillWayRechargeAlipay       BillWay = "recharge_alipay"
+	BillWayMissionTime          BillWay = "mission_time"
+	BillWayMissionTimePlanHour  BillWay = "mission_time_plan_hour"
+	BillWayMissionTimePlanDay   BillWay = "mission_time_plan_day"
+	BillWayMissionTimePlanWeek  BillWay = "mission_time_plan_week"
+	BillWayMissionTimePlanMonth BillWay = "mission_time_plan_month"
+	BillWayMissionCount         BillWay = "mission_count"
+	BillWayMissionHold          BillWay = "mission_hold"
+	BillWayMissionVolume        BillWay = "mission_volume"
+	BillWayActiveRegister       BillWay = "active_register"
+	BillWayActiveShare          BillWay = "active_share"
+	BillWayActiveRecharge       BillWay = "active_recharge"
+	BillWayTransferManual       BillWay = "transfer_manual"
+	BillWayFirstInviteRecharge  BillWay = "first_invite_recharge"
 )
 
 func (BillWay) Values() []string {
@@ -24,7 +27,10 @@ func (BillWay) Values() []string {
 		string(BillWayRechargeWechat),
 		string(BillWayRechargeAlipay),
 		string(BillWayMissionTime),
-		string(BillWayMissionTimePlan),
+		string(BillWayMissionTimePlanHour),
+		string(BillWayMissionTimePlanDay),
+		string(BillWayMissionTimePlanWeek),
+		string(BillWayMissionTimePlanMonth),
 		string(BillWayMissionCount),
 		string(BillWayMissionHold),
 		string(BillWayMissionVolume),
