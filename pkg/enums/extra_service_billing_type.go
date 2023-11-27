946846a85eb694184b8d33def9034e3adac4031d
sandwich3mz <806571058@qq.com>
2023-11-27 15:58:56 +0800

    fix: modify the enumeration

--- pkg/enums/extra_service_billing_type.go
+++ pkg/enums/extra_service_billing_type.go
@@ -5,15 +5,15 @@ type ExtraServiceBillingType string
 const (
 	ExtraServiceBillingTypeUnknown ExtraServiceBillingType = "unknown"
 	// ExtraServiceBillingTypeVPNTimePlanHour VPN 服务包时 - 按小时
-	ExtraServiceBillingTypeVPNTimePlanHour ExtraServiceBillingType = "vpn_time_plan_hour"
+	ExtraServiceBillingTypeVPNTimePlanHour ExtraServiceBillingType = "time_plan_hour"
 	// ExtraServiceBillingTypeVPNTimePlanDay  VPN 服务包时 - 按天
-	ExtraServiceBillingTypeVPNTimePlanDay ExtraServiceBillingType = "vpn_time_plan_day"
+	ExtraServiceBillingTypeVPNTimePlanDay ExtraServiceBillingType = "time_plan_day"
 	// ExtraServiceBillingTypeVPNTimePlanWeek VPN 服务包时 - 按周
-	ExtraServiceBillingTypeVPNTimePlanWeek ExtraServiceBillingType = "vpn_time_plan_week"
+	ExtraServiceBillingTypeVPNTimePlanWeek ExtraServiceBillingType = "time_plan_week"
 	// ExtraServiceBillingTypeVPNTimePlanMonth VPN 服务包时 - 按月
-	ExtraServiceBillingTypeVPNTimePlanMonth ExtraServiceBillingType = "vpn_time_plan_month"
+	ExtraServiceBillingTypeVPNTimePlanMonth ExtraServiceBillingType = "time_plan_month"
 	// ExtraServiceBillingTypeVPNTimePlanVolume VPN 服务按量，单次任务，内部计时 * 时间单价
-	ExtraServiceBillingTypeVPNTimePlanVolume ExtraServiceBillingType = "vpn_time_plan_volume"
+	ExtraServiceBillingTypeVPNTimePlanVolume ExtraServiceBillingType = "time_plan_volume"
 	// ExtraServiceBillingTypeVPNHold 按位，特点是开启期间具备某些功能，结束后失去功能
 	ExtraServiceBillingTypeVPNHold ExtraServiceBillingType = "hold"
 )
