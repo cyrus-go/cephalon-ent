2fd6496b9e4b0dccfed6e92abfd69144c1a12641
sandwich3mz <806571058@qq.com>
2023-11-28 16:41:30 +0800

    fix: modify the annotation

--- pkg/enums/extra_service_billing_type.go
+++ pkg/enums/extra_service_billing_type.go
@@ -4,15 +4,15 @@ type ExtraServiceBillingType string
 
 const (
 	ExtraServiceBillingTypeUnknown ExtraServiceBillingType = "unknown"
-	// ExtraServiceBillingTypeTimePlanHour VPN 服务包时 - 按小时
+	// ExtraServiceBillingTypeTimePlanHour 附加服务包时 - 按小时
 	ExtraServiceBillingTypeTimePlanHour ExtraServiceBillingType = "time_plan_hour"
-	// ExtraServiceBillingTypeTimePlanDay  VPN 服务包时 - 按天
+	// ExtraServiceBillingTypeTimePlanDay  附加服务包时 - 按天
 	ExtraServiceBillingTypeTimePlanDay ExtraServiceBillingType = "time_plan_day"
-	// ExtraServiceBillingTypeTimePlanWeek VPN 服务包时 - 按周
+	// ExtraServiceBillingTypeTimePlanWeek 附加服务包时 - 按周
 	ExtraServiceBillingTypeTimePlanWeek ExtraServiceBillingType = "time_plan_week"
-	// ExtraServiceBillingTypeTimePlanMonth VPN 服务包时 - 按月
+	// ExtraServiceBillingTypeTimePlanMonth 附加服务包时 - 按月
 	ExtraServiceBillingTypeTimePlanMonth ExtraServiceBillingType = "time_plan_month"
-	// ExtraServiceBillingTypeVolume VPN 服务按量，单次任务，内部计时 * 时间单价
+	// ExtraServiceBillingTypeVolume 附加服务按量，单次任务，内部计时 * 时间单价
 	ExtraServiceBillingTypeVolume ExtraServiceBillingType = "time_plan_volume"
 	// ExtraServiceBillingTypeHold 按位，特点是开启期间具备某些功能，结束后失去功能
 	ExtraServiceBillingTypeHold ExtraServiceBillingType = "hold"
