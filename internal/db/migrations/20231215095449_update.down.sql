cbeadbc971c6a46d19767532808af48187159cf5
huoayi <hyk130406@icloud.com>
2023-12-15 18:19:52 +0800

    fix: make default bills.proit_symbol_id == 3

---
+++ internal/db/migrations/20231215095449_update.down.sql
@@ -0,0 +1,2 @@
+-- reverse: modify "bills" table
+ALTER TABLE "bills" ALTER COLUMN "profit_symbol_id" SET DEFAULT 0;
---
+++ internal/db/migrations/20231215095449_update.up.sql
@@ -0,0 +1,2 @@
+-- modify "bills" table
+ALTER TABLE "bills" ALTER COLUMN "profit_symbol_id" SET DEFAULT 3;
