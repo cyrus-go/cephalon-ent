6960d874444f644186588a1fd810946a965e9813
sandwich3mz <806571058@qq.com>
2024-02-04 15:17:11 +0800

    feat: add bill way enum

--- pkg/enums/bill_way.go
+++ pkg/enums/bill_way.go
@@ -33,6 +33,7 @@ const (
 	BillWayWithdrawVX                BillWay = "withdraw_wechat"
 	BillWayWithdrawAlipay            BillWay = "withdraw_alipay"
 	BillWayBankCard                  BillWay = "withdraw_bank_card"
+	BillWayWithdrawRefund            BillWay = "withdraw_refund"
 	BillWayCdkExchange               BillWay = "cdk_exchange"
 	BillWayLottoPrize                BillWay = "lotto_prize"
 )
@@ -42,6 +43,7 @@ func (BillWay) Values() []string {
 		string(BillWayBankCard),
 		string(BillWayWithdrawAlipay),
 		string(BillWayWithdrawVX),
+		string(BillWayWithdrawRefund),
 		string(BillWayUnknown),
 		string(BillWayRechargeWechat),
 		string(BillWayRechargeAlipay),
