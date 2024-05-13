552a521a75918a2a9124087f08844f818a9e13f4
HzTTT <767067749@qq.com>
2024-05-13 16:44:49 +0800

    feat(user):add fooocus lan_que

--- pkg/enums/mission_category.go
+++ pkg/enums/mission_category.go
@@ -3,37 +3,38 @@ package enums
 type MissionCategory string
 
 const (
-	MissionCategoryUnknown    MissionCategory = "unknown"
-	MissionCategorySD         MissionCategory = "SD"
-	MissionCategoryJP         MissionCategory = "JP"
-	MissionCategoryWT         MissionCategory = "WT"
-	MissionCategoryJPDK       MissionCategory = "JP_DK"
-	MissionCategorySSH        MissionCategory = "SSH"
-	MissionCategorySDTOMATO   MissionCategory = "SD_TOMATO"
-	MissionCategorySDCMD      MissionCategory = "SD_CMD"
-	MissionCategorySDBingo    MissionCategory = "SD_BINGO"
-	MissionCategoryFooocus    MissionCategory = "FOOOCUS"
-	MissionCategoryTabby      MissionCategory = "TABBY"
-	MissionCategoryJpConda    MissionCategory = "JP_CONDA"
-	MissionCategorySDCat      MissionCategory = "SD_CAT"
-	MissionCategorySDFire     MissionCategory = "SD_FIRE"
-	MissionCategoryComfyui    MissionCategory = "COMFYUI"
-	MissionCategorySDXL       MissionCategory = "SD_XL"
-	MissionCategorySDChick    MissionCategory = "SD_CHICK"
-	MissionCategoryAscend     MissionCategory = "ASCEND"
-	MissionCategorySDWuShan   MissionCategory = "SD_WU_SHAN"
-	MissionCategorySDLang     MissionCategory = "SD_LANG"
-	MissionCategoryComfyuiKe  MissionCategory = "COMFYUI_KE"
-	MissionCategoryChatchat   MissionCategory = "CHATCHAT"
-	MissionCategoryLora       MissionCategory = "LORA"
-	MissionCategoryFooocusWu  MissionCategory = "FOOOCUS_WU"
-	MissionCategorySvdBack    MissionCategory = "SVD_BACK"
-	MissionCategorySDJi       MissionCategory = "SD_JI"
-	MissionCategorySDShangJin MissionCategory = "SD_SHANG_JIN"
-	MissionCategorySDXiaoChun MissionCategory = "SD_XIAO_CHUN"
-	MissionCategoryComfyuiWu  MissionCategory = "COMFYUI_WU"
-	MissionCategoryComfyuiLiu MissionCategory = "COMFYUI_LIU"
-	MissionCategoryWaiting    MissionCategory = "WAITING"
+	MissionCategoryUnknown       MissionCategory = "unknown"
+	MissionCategorySD            MissionCategory = "SD"
+	MissionCategoryJP            MissionCategory = "JP"
+	MissionCategoryWT            MissionCategory = "WT"
+	MissionCategoryJPDK          MissionCategory = "JP_DK"
+	MissionCategorySSH           MissionCategory = "SSH"
+	MissionCategorySDTOMATO      MissionCategory = "SD_TOMATO"
+	MissionCategorySDCMD         MissionCategory = "SD_CMD"
+	MissionCategorySDBingo       MissionCategory = "SD_BINGO"
+	MissionCategoryFooocus       MissionCategory = "FOOOCUS"
+	MissionCategoryFooocusLanQue MissionCategory = "FOOOCUS_LAN_QUE"
+	MissionCategoryTabby         MissionCategory = "TABBY"
+	MissionCategoryJpConda       MissionCategory = "JP_CONDA"
+	MissionCategorySDCat         MissionCategory = "SD_CAT"
+	MissionCategorySDFire        MissionCategory = "SD_FIRE"
+	MissionCategoryComfyui       MissionCategory = "COMFYUI"
+	MissionCategorySDXL          MissionCategory = "SD_XL"
+	MissionCategorySDChick       MissionCategory = "SD_CHICK"
+	MissionCategoryAscend        MissionCategory = "ASCEND"
+	MissionCategorySDWuShan      MissionCategory = "SD_WU_SHAN"
+	MissionCategorySDLang        MissionCategory = "SD_LANG"
+	MissionCategoryComfyuiKe     MissionCategory = "COMFYUI_KE"
+	MissionCategoryChatchat      MissionCategory = "CHATCHAT"
+	MissionCategoryLora          MissionCategory = "LORA"
+	MissionCategoryFooocusWu     MissionCategory = "FOOOCUS_WU"
+	MissionCategorySvdBack       MissionCategory = "SVD_BACK"
+	MissionCategorySDJi          MissionCategory = "SD_JI"
+	MissionCategorySDShangJin    MissionCategory = "SD_SHANG_JIN"
+	MissionCategorySDXiaoChun    MissionCategory = "SD_XIAO_CHUN"
+	MissionCategoryComfyuiWu     MissionCategory = "COMFYUI_WU"
+	MissionCategoryComfyuiLiu    MissionCategory = "COMFYUI_LIU"
+	MissionCategoryWaiting       MissionCategory = "WAITING"
 )
 
 func (obj MissionCategory) Values() []string {
@@ -48,6 +49,7 @@ func (obj MissionCategory) Values() []string {
 		string(MissionCategorySDCMD),
 		string(MissionCategorySDBingo),
 		string(MissionCategoryFooocus),
+		string(MissionCategoryFooocusLanQue),
 		string(MissionCategoryTabby),
 		string(MissionCategoryJpConda),
 		string(MissionCategorySDCat),
--- pkg/enums/mission_type.go
+++ pkg/enums/mission_type.go
@@ -18,6 +18,7 @@ const (
 	MissionTypeSdCmdTime          MissionType = "sd_cmd_time"
 	MissionTypeSdBingoTime        MissionType = "sd_bingo_time"
 	MissionTypeFooocusTime        MissionType = "fooocus_time"
+	MissionTypeFooocusLanQueTime  MissionType = "fooocus_lan_que_time"
 	MissionTypeTabbyTime          MissionType = "tabby_time"
 	MissionTypeJpCondaTime        MissionType = "jp_conda_time"
 	MissionTypeJpMlTime           MissionType = "jp_ml_time"
@@ -43,39 +44,40 @@ const (
 	MissionTypeComfyuiLiuTime     MissionType = "comfyui_liu_time"
 	MissionTypeWaitingTime        MissionType = "waiting_time" //等待任务
 
-	MissionTypeSdTimePlan         MissionType = "sd_time_plan"
-	MissionTypeWtTimePlan         MissionType = "wt_time_plan"
-	MissionTypeJpTimePlan         MissionType = "jp_time_plan"
-	MissionTypeJpDkTimePlan       MissionType = "jp_dk_time_plan"
-	MissionTypeSshTimePlan        MissionType = "ssh_time_plan"
-	MissionTypeSdTomatoTimePlan   MissionType = "sd_tomato_time_plan"
-	MissionTypeSdCmdTimePlan      MissionType = "sd_cmd_time_plan"
-	MissionTypeSdBingoTimePlan    MissionType = "sd_bingo_time_plan"
-	MissionTypeFooocusTimePlan    MissionType = "fooocus_time_plan"
-	MissionTypeTabbyTimePlan      MissionType = "tabby_time_plan"
-	MissionTypeJpCondaTimePlan    MissionType = "jp_conda_time_plan"
-	MissionTypeJpMlTimePlan       MissionType = "jp_ml_time_plan"
-	MissionTypeSdCatTimePlan      MissionType = "sd_cat_time_plan"
-	MissionTypeSdFireTimePlan     MissionType = "sd_fire_time_plan"
-	MissionTypeComfyuiTimePlan    MissionType = "comfyui_time_plan"
-	MissionTypeComfyuiProTimePlan MissionType = "comfyui_pro_time_plan"
-	MissionTypeJpDk3TimePlan      MissionType = "jp_dk_3_time_plan"
-	MissionTypeSdXlTimePlan       MissionType = "sd_xl_time_plan"
-	MissionTypeSdChickTimePlan    MissionType = "sd_chick_time_plan"
-	MissionTypeAscendTimePlan     MissionType = "ascend_time_plan"
-	MissionTypeSdWuShanTimePlan   MissionType = "sd_wu_shan_time_plan"
-	MissionTypeSdLangTimePlan     MissionType = "sd_lang_time_plan"
-	MissionTypeComfyuiKeTimePlan  MissionType = "comfyui_ke_time_plan"
-	MissionTypeChatchatTimePlan   MissionType = "chatchat_time_plan"
-	MissionTypeLoraTimePlan       MissionType = "lora_time_plan"
-	MissionTypeFooocusWuTimePlan  MissionType = "fooocus_wu_time_plan"
-	MissionTypeSvdBackTimePlan    MissionType = "svd_back_time_plan"
-	MissionTypeSdJiTimePlan       MissionType = "sd_ji_time_plan"
-	MissionTypeSdShangJinTimePlan MissionType = "sd_shang_jin_time_plan"
-	MissionTypeSdXiaoChunTimePlan MissionType = "sd_xiao_chun_time_plan"
-	MissionTypeComfyuiWuTimePlan  MissionType = "comfyui_wu_time_plan"
-	MissionTypeComfyuiLiuTimePlan MissionType = "comfyui_liu_time_plan"
-	MissionTypeWaitingTimePlan    MissionType = "waiting_time_plan" //等待任务
+	MissionTypeSdTimePlan            MissionType = "sd_time_plan"
+	MissionTypeWtTimePlan            MissionType = "wt_time_plan"
+	MissionTypeJpTimePlan            MissionType = "jp_time_plan"
+	MissionTypeJpDkTimePlan          MissionType = "jp_dk_time_plan"
+	MissionTypeSshTimePlan           MissionType = "ssh_time_plan"
+	MissionTypeSdTomatoTimePlan      MissionType = "sd_tomato_time_plan"
+	MissionTypeSdCmdTimePlan         MissionType = "sd_cmd_time_plan"
+	MissionTypeSdBingoTimePlan       MissionType = "sd_bingo_time_plan"
+	MissionTypeFooocusTimePlan       MissionType = "fooocus_time_plan"
+	MissionTypeFooocusLanQueTimePlan MissionType = "fooocus_lan_que_time_plan"
+	MissionTypeTabbyTimePlan         MissionType = "tabby_time_plan"
+	MissionTypeJpCondaTimePlan       MissionType = "jp_conda_time_plan"
+	MissionTypeJpMlTimePlan          MissionType = "jp_ml_time_plan"
+	MissionTypeSdCatTimePlan         MissionType = "sd_cat_time_plan"
+	MissionTypeSdFireTimePlan        MissionType = "sd_fire_time_plan"
+	MissionTypeComfyuiTimePlan       MissionType = "comfyui_time_plan"
+	MissionTypeComfyuiProTimePlan    MissionType = "comfyui_pro_time_plan"
+	MissionTypeJpDk3TimePlan         MissionType = "jp_dk_3_time_plan"
+	MissionTypeSdXlTimePlan          MissionType = "sd_xl_time_plan"
+	MissionTypeSdChickTimePlan       MissionType = "sd_chick_time_plan"
+	MissionTypeAscendTimePlan        MissionType = "ascend_time_plan"
+	MissionTypeSdWuShanTimePlan      MissionType = "sd_wu_shan_time_plan"
+	MissionTypeSdLangTimePlan        MissionType = "sd_lang_time_plan"
+	MissionTypeComfyuiKeTimePlan     MissionType = "comfyui_ke_time_plan"
+	MissionTypeChatchatTimePlan      MissionType = "chatchat_time_plan"
+	MissionTypeLoraTimePlan          MissionType = "lora_time_plan"
+	MissionTypeFooocusWuTimePlan     MissionType = "fooocus_wu_time_plan"
+	MissionTypeSvdBackTimePlan       MissionType = "svd_back_time_plan"
+	MissionTypeSdJiTimePlan          MissionType = "sd_ji_time_plan"
+	MissionTypeSdShangJinTimePlan    MissionType = "sd_shang_jin_time_plan"
+	MissionTypeSdXiaoChunTimePlan    MissionType = "sd_xiao_chun_time_plan"
+	MissionTypeComfyuiWuTimePlan     MissionType = "comfyui_wu_time_plan"
+	MissionTypeComfyuiLiuTimePlan    MissionType = "comfyui_liu_time_plan"
+	MissionTypeWaitingTimePlan       MissionType = "waiting_time_plan" //等待任务
 )
 
 func (obj MissionType) Values() []string {
@@ -104,6 +106,8 @@ func (obj MissionType) Values() []string {
 		string(MissionTypeSdBingoTimePlan),
 		string(MissionTypeFooocusTime),
 		string(MissionTypeFooocusTimePlan),
+		string(MissionTypeFooocusLanQueTime),
+		string(MissionTypeFooocusLanQueTimePlan),
 		string(MissionTypeTabbyTime),
 		string(MissionTypeTabbyTimePlan),
 		string(MissionTypeJpCondaTime),
