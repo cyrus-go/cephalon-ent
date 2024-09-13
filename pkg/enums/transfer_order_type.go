f0762df3b6dfa44ce8a13deb702cac77f2824540
cyrus <cyrus@cephalon.ai>
2024-09-13 15:54:10 +0800

    feat: add transfer order type recharge_airwallex

--- pkg/enums/transfer_order_type.go
+++ pkg/enums/transfer_order_type.go
@@ -3,17 +3,18 @@ package enums
 type TransferOrderType string
 
 const (
-	TransferOrderTypeUnknown        TransferOrderType = "unknown"
-	TransferOrderTypeRecharge       TransferOrderType = "recharge"
-	TransferOrderTypeRechargeVX     TransferOrderType = "recharge_vx"
-	TransferOrderTypeRefund         TransferOrderType = "recharge_refund"
-	TransferOrderTypeRechargeAlipay TransferOrderType = "recharge_alipay"
-	TransferOrderTypeManual         TransferOrderType = "manual"
-	TransferOrderTypeWithdraw       TransferOrderType = "withdraw"
-	TransferOrderTypeWithdrawWX     TransferOrderType = "withdraw_vx"
-	TransferOrderTypeWithdrawAlipay TransferOrderType = "withdraw_alipay"
-	TransferOrderTypeBankCard       TransferOrderType = "withdraw_bank_card"
-	TransferOrderTypeCompany        TransferOrderType = "withdraw_company"
+	TransferOrderTypeUnknown           TransferOrderType = "unknown"
+	TransferOrderTypeRecharge          TransferOrderType = "recharge"
+	TransferOrderTypeRechargeVX        TransferOrderType = "recharge_vx"
+	TransferOrderTypeRefund            TransferOrderType = "recharge_refund"
+	TransferOrderTypeRechargeAlipay    TransferOrderType = "recharge_alipay"
+	TransferOrderTypeRechargeAirwallex TransferOrderType = "recharge_airwallex" // 使用 airwallex 付款
+	TransferOrderTypeManual            TransferOrderType = "manual"
+	TransferOrderTypeWithdraw          TransferOrderType = "withdraw"
+	TransferOrderTypeWithdrawWX        TransferOrderType = "withdraw_vx"
+	TransferOrderTypeWithdrawAlipay    TransferOrderType = "withdraw_alipay"
+	TransferOrderTypeBankCard          TransferOrderType = "withdraw_bank_card"
+	TransferOrderTypeCompany           TransferOrderType = "withdraw_company"
 )
 
 func (TransferOrderType) Values() []string {
@@ -25,6 +26,7 @@ func (TransferOrderType) Values() []string {
 		string(TransferOrderTypeRecharge),
 		string(TransferOrderTypeRechargeVX),
 		string(TransferOrderTypeRechargeAlipay),
+		string(TransferOrderTypeRechargeAirwallex),
 		string(TransferOrderTypeManual),
 		string(TransferOrderTypeWithdraw),
 		string(TransferOrderTypeRefund),
