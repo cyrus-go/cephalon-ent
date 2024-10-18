1ed6ea60498fd9e0c70526c2d83b71da18df9677
cyrus <cyrus@cephalon.ai>
2024-10-18 14:19:54 +0800

    fix: db version bug

--- internal/db/migrations/20240928064217_update.down.sql
+++
@@ -1,4 +0,0 @@
--- reverse: create index "user_phone_deleted_at_email_github_id_google_id" to table: "users"
-DROP INDEX "user_phone_deleted_at_email_github_id_google_id";
--- reverse: drop index "user_github_id" from table: "users"
-CREATE INDEX "user_github_id" ON "users" ("github_id");
--- internal/db/migrations/20240928064217_update.up.sql
+++
@@ -1,4 +0,0 @@
--- drop index "user_github_id" from table: "users"
-DROP INDEX "user_github_id";
--- create index "user_phone_deleted_at_email_github_id_google_id" to table: "users"
-CREATE UNIQUE INDEX "user_phone_deleted_at_email_github_id_google_id" ON "users" ("phone", "deleted_at", "email", "github_id", "google_id");
